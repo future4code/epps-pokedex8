720f3543d4e67334af14ab5d07e05c1673bf9e74
Allison Araujo <allisonaraujo94@outlook.com>
2021-02-10 17:23:46 -0300

    Estilização do Button

--- projeto-pokedex/src/Components/Buttons/styled.js
+++ projeto-pokedex/src/Components/Buttons/styled.js
@@ -1,7 +1,22 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const Button = styled.div`
-    padding:8px;
-    background:#ffffff;
-    margin-right:4px;
-`
+export const Button = styled.button`
+  border: none;
+  outline: none !important;
+  cursor: pointer;
+  padding: 8px;
+  font-weight: bold;
+  transition: 0.5s;
+  background-color: ${(props) => props.bgColor};
+  color: blanchedalmond;
+
+  :hover {
+    background-color: lightslategrey;
+    margin-top: -8px;
+  }
+
+  width: 100%;
+  border-bottom-left-radius: ${props=>props.radiusBL};
+  border-bottom-right-radius: ${props=>props.radiusBR};
+  margin-bottom: -9px;
+`;
