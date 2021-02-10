1b9b2f8437a0964e0fe7df4f79400e013cb8bac1
Allison Araujo <allisonaraujo94@outlook.com>
2021-02-10 17:31:15 -0300

    Function do goToPage por props antigo

--- projeto-pokedex/src/Components/Buttons/Button.js
+++ projeto-pokedex/src/Components/Buttons/Button.js
@@ -5,7 +5,7 @@ function ButtonProps(props) {
                 bgColor={props.bgColor}
                 radiusBL={props.radiusL}
                 radiusBR={props.radiusR}
-            
+                onClick={props.goToPage}
             > {props.title} </Button>
 }
 
