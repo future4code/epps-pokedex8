34d71131dd594ffcd83ba15a424d0dd68485ee82
franciane-brito <francianebrito99@gmail.com>
2021-02-10 00:35:41 -0300

    Atualizando style da pagina de detalhes

--- projeto-pokedex/src/Screens/PokemonDetailsPage.js
+++ projeto-pokedex/src/Screens/PokemonDetailsPage.js
@@ -1,106 +1,128 @@
 import React, { useState } from "react"
 import styled from 'styled-components'
-import Seta from "../images/seta-para-direita.png"
+import ImagePoke from "../Img/pokedex.png"
 
 const ContainerContent = styled.div`
-display: grid;
+display: flex;
 justify-content: center;
-grid-template-columns: 1fr 1fr 1fr;
 align-items: center;
-border: solid 2px #f0f0f0;
 border-radius: 10px;
 height: 100vh;
+font-family: "Lato";
 `
+
 const ContainerDetails = styled.div`
-margin: 10px;
-border: solid 2px #f0f0f0;
+width: 30vw;
+height: 40vh;
+margin: 30px;
+color: #F8CF2D;
+font-size: 20px;
+background-color: #423D94;
 border-radius: 10px;
 padding: 20px; 
-box-shadow: 0px 0px 50px 1px #DDDDDD;
+box-shadow: 0 3px 15px rgb(100 100 100 / 50%);
+&:hover{
+  transition: transform 1s;
+  transform: scale(1.01);
+}
 `
 const ContainerType = styled.div`
-background-color: #f0f0f0;
-margin: 10px;
-border: solid 2px #f0f0f0;
+width: 30vw;
+height: 44vh;
+margin: 30px;
+font-size: 20px;
+color: #423D94;
+background-color: #3F3C93;
 border-radius: 10px;
 padding: 20px; 
-box-shadow: 0px 0px 50px 1px #DDDDDD;
+box-shadow: 0 3px 15px rgb(100 100 100 / 50%);
+background-color: #A3F3CE;
+&:hover{
+  transition: transform 0.8s;
+  transform: scale(1.01)  ;
+}
 `
 const ImageOne = styled.div`
-display: flex;
-align-items: center;
-justify-content: flex-end;
-border: solid 2px #f0f0f0;
+margin: 40px;
+color: #423D94;
+background-color: #50A7D7;
+box-shadow: 0 3px 18px rgb(100 100 100 / 50%); 
+width: 27vw;
+height: 70vh;
+transition: transform 0.8s;
+transform-style: preserve-3d;
+position: relative;
 border-radius: 10px;
-padding: 20px; 
-width: 25vw;
-height: 65vh;
-box-shadow: 0px 0px 50px 1px #DDDDDD;
+&:hover{
+  transform: rotateY(180deg);
+  transition: transform 0.8s;
+}
 `
-const ImageTwo = styled.div`
-display: flex;
-align-items: center;
-justify-content: flex-end;
-border: solid 2px #f0f0f0;
-border-radius: 10px;
-padding: 20px; 
-width: 25vw;
-height: 65vh;
-box-shadow: 0px 0px 50px 1px #DDDDDD;
-`
+
 const DivType = styled.div`
 margin: 10px;
-border: solid 2px #f0f0f0;
 border-radius: 10px;
 padding: 20px; 
-background-color: #FFF;
+background-color: #D6F9EB;
 `
 const DivMove = styled.div`
 margin: 10px;
-border: solid 2px #f0f0f0;
 border-radius: 10px;
 padding: 20px; 
-background-color: #FFF;
+background-color: #D6F9EB;
 `
-const Button = styled.div`
-position: absolute;
-&:hover{
-  transform: scale(1.3)
-  
 
-}
+const Front = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-around;
+flex-direction: column;
+border-radius: 10px;
+padding: 20px; 
+width: 25vw;
+height: 65vh;
+box-shadow: 0 3px 18px rgb(100 100 100 / 50%);
+position: absolute;
+width: 100%;
+height: 100%;
+backface-visibility: hidden;
 `
-const StyledSeta = styled.img`
-width: 3vw;
-height: 5vh;
+
+const Back = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-around;
+flex-direction: column;
+border-radius: 10px;
+padding: 20px; 
+width: 25vw;
+height: 65vh;
+box-shadow: 0 3px 18px rgb(100 100 100 / 50%);
+position: absolute;
+width: 100%;
+height: 100%;
+backface-visibility: hidden;
+transform: rotateY(180deg);
 `
 
 const PokemonDetailsPage = () => {
-  const [images, setImages] = useState(true)
-
-const image = () => {
-  if(images) {
-return(
- 
-  <ImageOne>
-  <img src="https://picsum.photos/350" />
-  <Button onClick={() => setImages(false)}><StyledSeta src={Seta} /></Button>
-  </ImageOne>
- )
-  } else {
-    return (
-      <ImageTwo>
-      <img src="https://picsum.photos/350" />
-      <Button onClick={() => setImages(true)}><StyledSeta src={Seta} /></Button>
-      </ImageTwo>
-    )
-  }
-} 
+  
   return (
     <ContainerContent>
-      {image()}
+      <ImageOne>
+        <Front>
+          <h2>Imagem frontal</h2>
+          <img src={ImagePoke} />
+        </Front>
 
-      <ContainerDetails>
+        <Back>
+        <h2>Imagem de costas</h2>
+          <img src="https://picsum.photos/350" />
+        </Back>
+      </ImageOne>
+
+      <div>
+        <ContainerDetails>
           <h3>Stats</h3>
           <p>HP: </p>
           <p>Attack: </p>
@@ -108,27 +130,26 @@ return(
           <p>special-attack: </p>
           <p>special-defence: </p>
           <p>speed: </p>
-      </ContainerDetails>
+        </ContainerDetails>
 
-      <ContainerType>
-        <DivType>
-          <p>Type 1</p>
-          <p>Type 2</p>
-        </DivType>
+        <ContainerType>
+          <DivType>
+            <p>Type 1</p>
+            <p>Type 2</p>
+          </DivType>
 
-        <DivMove>
-          <h3>Moves</h3>
-          <p>move name 1</p>
-          <p>move name 2</p>
-          <p>move name 3</p>
+          <DivMove>
+            <h3>Moves</h3>
+            <p>move name 1</p>
+            <p>move name 2</p>
+            <p>move name 3</p>
 
-        </DivMove>
-      </ContainerType>
-
+          </DivMove>
+        </ContainerType>
+      </div>
 
     </ContainerContent>
   );
 };
 
 export default PokemonDetailsPage;
-
